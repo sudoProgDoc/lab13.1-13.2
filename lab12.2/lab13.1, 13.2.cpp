f700b4c8168fd59b1008310c00c1eaba4119cb02
Kris <sudoprogdoc@gmail.com>
2025-01-05 15:21:24 +0700

    added 13.3

--- lab12.2/lab13.1, 13.2.cpp
+++ lab12.2/lab13.1, 13.2.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int solver(int n, int m, float** A, float* X) {
     int i, j, k, v;
+    bool w;
     float c, eps = 0.000001;
 
-    for (i = 0; i < n - 1; i++) {
+    for (i = 0; i < n - 1; i++)
+    {
         v = i;
         for (j = i + 1; j < n; j++)
             if (abs(A[j][i]) > abs(A[v][i])) v = j;
-        if (v != i)
-        {
+
+        if (v != i) {
             for (j = 0; j <= n; j++) {
                 c = A[i][j];
                 A[i][j] = A[v][j];
@@ -18,7 +21,7 @@ int solver(int n, int m, float** A, float* X) {
             }
         }
 
-        //приведение к ступенчатому виду
+        // приведение к ступенчатому виду
         for (k = i + 1; k < n; k++) {
             c = A[k][i] / A[i][i];
             for (j = i; j <= n; j++)
@@ -26,34 +29,51 @@ int solver(int n, int m, float** A, float* X) {
         }
     }
 
+    // запись промежуточного результата в файл
     FILE* fm = fopen("ou.txt", "at");
-    for (i = 0; i < m; i++)
-    {
+    for (i = 0; i < m; i++) {
         for (j = 0; j <= n; j++) fprintf(fm, "%8.3f", A[i][j]);
         fprintf(fm, "\n");
     }
     fclose(fm);
 
-    //обратный ход
+    // Обратный ход
     for (i = n - 1; i >= 0; i--) {
+        if (abs(A[i][i]) < eps) {
+            // Проверка на наличие свободных переменных
+            w = true;
+            for (j = 0; j < n; j++)
+            {
+                if (abs(A[i][j]) > eps)
+                {
+                    w = false;
+                    break;
+                }
+            }
+            if (w && abs(A[i][n]) > eps) return 0; // Нет решений
+            continue; // Свободная переменная
+        }
         for (k = 0; k < i; k++) {
             c = A[k][i] / A[i][i];
             A[k][n] -= c * A[i][n];
         }
     }
 
-    
+    // Решение
+    for (i = 0; i < n; i++) {
+        if (abs(A[i][i]) > eps) {
+            X[i] = A[i][n] / A[i][i];
+        }
+        else {
+            X[i] = NAN;
+        }
+    }
 
-    //решение
-    for (i = 0; i < n; i++)
-        X[i] = A[i][n] / A[i][i];
-    if (isnan(X[0])) return 0;
     return 1;
 }
 
 int main() {
     FILE* fl = fopen("test1.txt", "rt");
-    
 
     float* solution, ** a, f;
     int n, m, i, j, g;
@@ -63,30 +83,30 @@ int main() {
     a = new float* [m];
     solution = new float[n];
     for (i = 0; i < n; i++) solution[i] = 0;
-    for (i = 0; i < m; i++) {
+    for (i = 0; i < m; i++)
+    {
         a[i] = new float[n + 1];
-        for (j = 0; j <= n; j++) {
+        for (j = 0; j <= n; j++)
+        {
             fscanf(fl, "%f", &f);
             a[i][j] = f;
         }
     }
+    fclose(fl);
 
     g = solver(n, m, a, solution);
-    if (g) {
-        for (i = 0; i < n; i++)
-            printf("%8.3f", solution[i]);
-        printf("\n");
-    }
-    else {
-        printf("ERROR\n");
-    }
 
+    if (g)
+    {
+        for (i = 0; i < n; i++)
+            if (isnan(solution[i])) cout << "x[" << i << "] is a free variable." << endl;
+            else printf("x[%d] = %8.3f\n", i + 1, solution[i]);
+    }
+    else printf("ERROR: No solutions\n");
 
     for (i = 0; i < m; i++) {
         delete[] a[i];
     }
     delete[] a;
     delete[] solution;
-
-    fclose(fl);
 }
